c8ba39490bb87fb593f7074fdacc15b2c913839e
todorkurtev <todor9393@gmail.com>
2016-10-15 14:46:02 +0300

    Js core exam excersise

---
+++ Exam Prep/X-Remove.js
@@ -0,0 +1,41 @@
+function xRemover(arr) {
+    let matrix = [];
+    let resultMatrix = [];
+
+    arr.forEach(function (string) {
+        matrix.push(string.toLowerCase().split(''));
+        resultMatrix.push(string.split(''))
+    });
+
+    for (let row = 0; row < matrix.length - 2; row++) {
+        for(let col = 0; col <matrix[row].length; col++ ){
+            let char = matrix[row][col];
+            let isX = matrix[row][col + 2] == char &&
+                    matrix[row + 1][col + 1] == char &&
+                    matrix[row + 2][col] == char &&
+                    matrix[row + 2][col + 2] == char;
+
+            if(isX){
+                resultMatrix[row][col] = " ";
+                resultMatrix[row][col + 2] = " ";
+                resultMatrix[row + 1][col + 1] = " ";
+                resultMatrix[row + 2][col] = " ";
+                resultMatrix[row + 2][col +2] = " ";
+            }
+        }
+    }
+    let resultArray = [];
+    resultMatrix.forEach(function(str) {
+        resultArray.push(str.join('').split(" ").join(''))
+    });
+    resultArray.forEach(function (item) {
+        console.log(item);
+    });
+}
+
+xRemover([
+    "8888888",
+    "08*8*80",
+    "808*888",
+    "0**8*8?"
+]);
---
+++ Exam Prep/totoNumbers.js
@@ -0,0 +1,7 @@
+for (let i1 = 1; i1 <= 44; i1++)
+    for (let i2 = i1 + 1; i2 <= 45; i2++)
+        for (let i3 = i2 + 1; i3 <= 46; i3++)
+            for (let i4 = i3 + 1; i4 <= 47; i4++)
+                for (let i5 = i4 + 1; i5 <= 48; i5++)
+                    for (let i6 = i5 + 1; i6 <= 49; i6++)
+                        console.log(i1, i2, i3, i4, i5, i6);
