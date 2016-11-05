11eea7eaa6e5685e5670e24b6ad693a05439e4e9
todorkurtev <todor9393@gmail.com>
2016-11-05 18:04:26 +0200

    JS

---
+++ AdvancedJavascript/Exercises Classes and Their Members/Tickets.js
@@ -0,0 +1,11 @@
+function tickets() {
+    class tickets{
+        constructor(destination, price, status){
+            this.destination = destination;
+            this.price = price;
+            this.status = status
+
+        }
+
+    }
+}
