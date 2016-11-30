b3e4246ffd74f6db93153189d7317cc94d13ab48
todorkurtev <todor9393@gmail.com>
2016-11-30 18:00:46 +0200

    Prodavachnik test

--- JS apps/Prodavachnik - Skeleton/scripts/prodavachnik.js
+++ JS apps/Prodavachnik - Skeleton/scripts/prodavachnik.js
@@ -1,3 +1,19 @@
-function startApp() {
-    documet.getElementsById("content").innerHTML''
+function startApp(){
+
+    function showHideMenuLinks() {
+        $("#menu a").hide();
+
+        if (sessionStorage.getItem("authToken")) {
+            //Logged In User:
+            $("#linkHome").show();
+            $("#linkListAds").show();
+            $("#linkCreateAd").show();
+            $("#linkLogout").show();
+        } else {
+            //Not Logged In User:
+            $("#linkHome").show();
+            $("#linkLogin").show();
+            $("#linkRegister").show();
+        }
+    }
 }
